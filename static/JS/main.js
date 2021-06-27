9730690e1b89dacee6fe73a410dfc8aa165ddef0
Moringa School <kelvin.bett@student.moringaschool.com>
2021-06-27 14:11:34 +0300

    js prototype of pizza type

--- static/JS/main.js
+++ static/JS/main.js
@@ -1,4 +1,13 @@
-function pizza(quantity, size) {
-    this.quantity = quantity;
+//business logic
+function pizza(pizzatype, size) {
+    this.pizzatype = pizzatype;
     this.size = size;
+}
+pizza.prototype.quantityBasedAmount = function() {
+    if (this.quantity.length != 0) {
+        var pizzatypePrice = this.quantity.length * 7;
+        return pizzatypePrice;
+    } else {
+        alert(" kindly choose your pizza type")
+    }
 }
